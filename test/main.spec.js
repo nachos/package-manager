e805165130a3bc477c85cf7cf6f9b8fdb521ab28
Omri Litov <omrilitov@gmail.com>
2015-07-24 14:53:39 +0300

    Refactor whole package-manager
    Added some tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -5,31 +5,18 @@ var expect = chai.expect;
 
 chai.use(require('chai-as-promised'));
 
-describe('package-manager', function () {
-  describe('exports', function () {
-    var packageManager = require('../lib');
+describe('exports', function () {
+  var packageManager = require('../lib');
 
-    it('should be an object', function () {
-      return expect(packageManager).to.be.an.object;
-    });
-
-    it('should have all methods', function () {
-    });
+  it('should be an object', function () {
+    return expect(packageManager).to.be.an.object;
   });
 
-  describe('link', function () {
-    describe('without parameters', function () {
-      var packageManager = require('../lib');
-
-      it('should be rejected with TypeError', function () {
-        return expect(packageManager.link()).to.eventually.be.rejectedWith(TypeError);
-      });
-    });
-
-    it('should be awesome', function () {
-      var packageManager = require('../lib');
-
-      return expect(packageManager.link('source')).to.be.empty;
-    });
+  it('should have all methods', function () {
+    expect(packageManager.install).to.be.a('function');
+    expect(packageManager.uninstall).to.be.a('function');
+    expect(packageManager.link).to.be.a('function');
+    expect(packageManager.login).to.be.a('function');
+    expect(packageManager.publish).to.be.a('function');
   });
 });
---
+++ test/uninstall.spec.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var chai = require('chai');
+var sinon = require('sinon');
+var Q = require('Q');
+var expect = chai.expect;
+var mockery = require('mockery');
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
+
+describe('login', function () {
+  describe('with invalid parameters', function () {
+    var packageManager = require('../lib');
+
+    it('should be rejected with empty data', function () {
+      return expect(packageManager.login()).to.eventually.be.rejectedWith(TypeError);
+    });
+
+    it('should be rejected with invalid email', function () {
+      return expect(packageManager.login({password: 'password'})).to.eventually.be.rejectedWith(TypeError);
+    });
+
+    it('should be rejected with invalid password', function () {
+      return expect(packageManager.login({email: 'email'})).to.eventually.be.rejectedWith(TypeError);
+    });
+  });
+
+  describe('with valid parameters', function () {
+    var packageManager;
+
+    describe('with correct data', function () {
+      beforeEach(function () {
+        var packageManagerMock = function () {
+          return {
+            connect: sinon.stub().returns(Q.resolve())
+          };
+        };
+
+        mockery.registerMock('nachos-server-api', packageManagerMock);
+        mockery.enable({
+          useCleanCache: true,
+          warnOnReplace: false,
+          warnOnUnregistered: false
+        });
+
+        packageManager = require('../lib');
+      });
+
+      afterEach(function () {
+        mockery.deregisterMock('nachos-server-api');
+        mockery.disable();
+      });
+
+      it('should login successfully', function () {
+        return expect(packageManager.login({email: 'email', password: 'password'})).to.eventually.be.fulfilled;
+      });
+    });
+
+    describe('with incorrect data', function () {
+      beforeEach(function () {
+        var packageManagerMock = function () {
+          return {
+            connect: sinon.stub().returns(Q.reject())
+          };
+        };
+
+        mockery.registerMock('nachos-server-api', packageManagerMock);
+        mockery.enable({
+          useCleanCache: true,
+          warnOnReplace: false,
+          warnOnUnregistered: false
+        });
+
+        packageManager = require('../lib');
+      });
+
+      afterEach(function () {
+        mockery.deregisterMock('nachos-server-api');
+        mockery.disable();
+      });
+
+      it('should not login', function () {
+        return expect(packageManager.login({email: 'email', password: 'password'})).to.eventually.be.rejected;
+      });
+    });
+  });
+});
